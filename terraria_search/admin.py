6d0f87ad1e75ab7397eb8f849e73a6ef177988f7
Shintakumi <shincode0712@gmail.com>
2021-06-29 23:06:11 +0900

    add a database table 3

--- terraria_search/admin.py
+++ terraria_search/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Items, ParentCategory
 
 # Register your models here.
+admin.site.register(ParentCategory)
+admin.site.register(Items)
+admin.site.register(Category)
--- terraria_search/models.py
+++ terraria_search/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import AutoField
+
+
+class ParentCategory(models.Model):
+  name = models.CharField(max_length=255)
+
+  def __str__(self):
+    return self.name
+
+class Category(models.Model):
+  name = models.CharField(max_length=255)
+  parentCategory_id = models.ForeignKey(ParentCategory, on_delete=CASCADE, null=True)
+
+  def __str__(self):
+    return self.name
+
+class Items(models.Model):
+  name = models.CharField(max_length=255)
+  parentCategory_id = models.ForeignKey(ParentCategory, on_delete=CASCADE, null=True)
+  category = models.ManyToManyField(Category, related_name='item')
+  needed_furniture = models.CharField(max_length=255)
+  needed_material = models.CharField(max_length=255)
+  how_to_get = models.TextField()
+
+  def __str__(self):
+    return self.name
+
+
+
 
 
